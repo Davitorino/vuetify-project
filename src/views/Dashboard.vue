9ba8c33516e432b66456a28d624451a6c6b53d41
Davi Vitorino <davitorino26@gmail.com>
2022-03-14 21:53:48 -0300

    feat: new components and layout grid additions

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,57 +1,105 @@
 <template>
-  <div>
+  <v-container>
     <h1>Dashboard</h1>
-     <v-data-table
-      :headers="headers"
-      :items="employees"
-      :items-per-page="5"
-      class="elevation-1"
-      @click:row="selectRow"
-    />
+
+    <v-row>
+      <v-col  
+        v-for="sale in sales"
+        :key="`${sale.title}`"
+        cols="12"
+        md="4"
+      >
+        <SalesGraph :sale="sale" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        v-for="statistic in statistics"
+        :key="`${statistic.title}`"
+        cols="12"
+        md="6"
+        lg="3"
+      >
+        <StatisticCard
+          :statistic="statistic"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        cols="12"
+        md="8"
+      >
+        <EmployeesTable
+          :employees="employees"
+          @select-employee="setEmployee"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
     <v-snackbar
       v-model="snackbar"
+      :left="$vuetify.breakpoint.lgAndUp"
     >
-      You have selected {{ currentItem }}
-      <template v-slot:action="{ attrs }">
-        <v-btn
-          color="pink"
-          text
-          v-bind="attrs"
-          @click="snackbar = false"
-        >
-          Close
-        </v-btn>
-      </template>
+      You have selected {{ selectedEmployee.name }},
+      {{ selectedEmployee.title }}
+      <v-btn
+        color="pink"
+        text
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
     </v-snackbar>
-  </div>
+  </v-container>
 </template>
 
 <script>
+import EmployeesTable from '../components/EmployeesTable'
+import EventTimeline from '../components/EventTimeline'
+import SalesGraph from '../components/SalesGraph'
+import StatisticCard from '../components/StatisticCard'
+
 import employeesData from '../data/employees.json'
+import timelineData from '../data/timeline.json'
+import salesData from '../data/sales.json'
+import statisticsData from '../data/statistics.json'
 
 export default {
-  data () {
+  name: 'DashboardPage',
+  components: {
+    EmployeesTable,
+    EventTimeline,
+    SalesGraph,
+    StatisticCard
+  },
+  data() {
     return {
-      currentItem: '',
-      snackbar: false,
-      headers: [
-        { text: 'Employee ID', value: 'id' },
-        { text: 'Name', value: 'name' },
-        { text: 'Position Title', value: 'title' },
-        { text: 'Salary', value: 'salary' },
-      ],
       employees: employeesData,
+      sales: salesData,
+      selectedEmployee: {
+        name: '',
+        title: ''
+      },
+      snackbar: false,
+      statistics: statisticsData,
+      timeline: timelineData
     }
   },
   methods: {
-    selectRow(event) {
+    setEmployee(event) {
       this.snackbar = true
-      this.currentItem = event.name
+      this.selectedEmployee.name = event.name
+      this.selectedEmployee.title = event.title
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
